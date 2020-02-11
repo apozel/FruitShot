20a93b54f3411780044228c44bf7685b7b7a8cf7
apozel <52205094+apozel@users.noreply.github.com>
2020-02-11 13:55:24 +0100

    first commit pour la creation du pmu

---
+++ app/src/main/java/fr/isen/myapplication/pmu/PmuActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.myapplication.pmu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PmuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pmu)
+    }
+}
