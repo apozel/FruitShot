b9f4e80af208b978e6c299a70f274d04f6e979b1
RemiFontan2 <remifontann@gmail.com>
2020-02-11 23:28:40 +0100

    Merge branch 'master' of https://github.com/apozel/FruitShot
    # Conflicts:
    #	.idea/codeStyles/Project.xml

--- app/src/main/java/fr/isen/myapplication/MainActivity.kt
+++ app/src/main/java/fr/isen/myapplication/MainActivity.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        purpleButton.setOnClickListener {
+            val intent = Intent(this, PurpleActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
 
         purpleButton.setOnClickListener {
             val intent = Intent(this, PurpleActivity::class.java)
