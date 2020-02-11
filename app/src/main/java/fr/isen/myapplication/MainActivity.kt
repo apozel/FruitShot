08e8e79f3194cd2e23d60f5ecef526202cec6a86
RemiFontan2 <remifontann@gmail.com>
2020-02-11 16:23:58 +0100

    intilial purple

--- app/src/main/java/fr/isen/myapplication/MainActivity.kt
+++ app/src/main/java/fr/isen/myapplication/MainActivity.kt
@@ -1,12 +1,21 @@
 package fr.isen.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        purpleButton.setOnClickListener {
+            val intent = Intent(this, PurpleActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
