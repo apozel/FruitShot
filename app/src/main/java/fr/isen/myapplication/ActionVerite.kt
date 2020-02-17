c802d0da6e1eee3878f8dc8acb5488b451afe945
chloecostes <chloe.costes9@gmail.com>
2020-02-17 13:41:57 +0100

    Merge branch 'Chloe'

--- app/src/main/java/fr/isen/myapplication/ActionVerite.kt
+++ app/src/main/java/fr/isen/myapplication/ActionVerite.kt
@@ -2,28 +2,49 @@ package fr.isen.myapplication
 
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_action_verite.*
+import com.google.firebase.database.DatabaseError
+import kotlin.random.Random
+
 
 class ActionVerite : AppCompatActivity() {
 
-   /* val database = FirebaseDatabase.getInstance()
-    val myRef = database.getReference("message")
 
-    myRef.setValue("Hello, World!")*/
-
+    private val TAG = "ActionVeriteActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_action_verite)
 
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("Action")
+        val actionList = arrayListOf<String>()
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                dataSnapshot.children.forEach {
+                    val value = it.getValue(String::class.java)
+                    Log.d(TAG, "Value is: $value")
+                    actionList.add(value ?: "")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
 
         ActionButton.setOnClickListener {
-            Toast.makeText(this , "Vous avez choisi action", Toast.LENGTH_LONG).show();
-
-
+            Toast.makeText(this , "Vous avez choisi action", Toast.LENGTH_LONG).show()
+            val randomIndexList = Random.nextInt(actionList.size)
+            val element = actionList[randomIndexList]
+            questionDisplay.text = element
         }
 
         VeriteButton.setOnClickListener {
