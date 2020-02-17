fe095d0944033146219cb6bb170cad22c660695f
apozel <52205094+apozel@users.noreply.github.com>
2020-02-17 15:01:59 +0100

    slide carte

--- app/src/main/java/fr/isen/myapplication/pmu/CardPackage.kt
+++ app/src/main/java/fr/isen/myapplication/pmu/CardPackage.kt
@@ -1,18 +1,19 @@
 package fr.isen.myapplication.pmu
 
 class CardPackage {
-    init {
+
+    fun getNewCardPackage(): ArrayList<Card>{
         var listOfCardColor: Array<CardColor> =
             arrayOf<CardColor>(CardColor.STRAWBERRY, CardColor.WATERMELON, CardColor.LEMON, CardColor.BANANA)
         for (color in listOfCardColor) {
-            for (i in 0..10) {
+            for (i in 1..10) {
                 cardList.add(Card(color, "${i}"))
             }
             cardList.add(Card(color,"J"))
             cardList.add(Card(color,"Q"))
             cardList.add(Card(color,"R"))
         }
-
+        return cardList
     }
 
     companion object {
